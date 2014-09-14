7063329500d62ad655f62987af61f681a8f7c0e2
Alex Schneider <aschne19@lion.lmu.edu>
2014-09-14 16:11:34 -0700

    Add Soccer Scores

---
+++ homework1/fifa2014group.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+import re
+import sys
+import requests
+from operator import itemgetter, attrgetter
+
+
+
+def getAllTeams(group):
+    r = requests.get('http://worldcup.kimonolabs.com/api/teams?apikey=KERbxAUfDYovbQnn9pR3pbLWEMRp47AQ&group={group}&fields=name,wins,losses,draws,goalsDiff'.format(group=group))
+    return r.json()
+
+def displayTeams(teams):
+    # https://wiki.python.org/moin/HowTo/Sorting
+    print teams
+    sorted_teams = sorted(teams, cmp=comparator)
+    print 'Name             W  D  L'
+    for team in sorted_teams:
+        print '{name:<17}{wins}  {draws}  {losses}'.format(**team)
+
+def comparator(key1, key2):
+    if key1['wins'] != key2['wins']:
+        return -cmp(key1['wins'], key2['wins'])
+    elif key1['draws'] != key2['draws']:
+        return -cmp(key1['draws'], key2['draws'])
+    else:
+        return cmp(key1['goalsDiff'], key2['goalsDiff'])
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1 or re.match('^[^A-H]$', sys.argv[1]):
+        print "Need just one commandline argument, A...H"
+    else:
+        group = sys.argv[1]
+        teams = getAllTeams(group)
+        displayTeams(teams)
