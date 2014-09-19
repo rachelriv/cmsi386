1add13595dd4eec500e024803c303e3227277ce8
Rachel Rivera <rachelriv94@yahoo.com>
2014-09-18 21:35:24 -0600

    a little bit of refactoring

--- homework2/warmup.js
+++ homework2/warmup.js
@@ -1,69 +1,66 @@
-module.exports = {
-    change: function(startingValue) {
-        if (startingValue < 0) {
-            throw new RangeError('amount cannot be negative');
-        }
-        var changeArr = [];
-        var quarterVal = 25,
-            dimeVal = 10,
-            nickelVal = 5;
-        value = startingValue;
-        changeArr.push(Math.floor(value / quarterVal));
-        value %= quarterVal;
-        changeArr.push(Math.floor(value / dimeVal));
-        value %= dimeVal;
-        changeArr.push(Math.floor(value / nickelVal));
-        changeArr.push(value % nickelVal);
-        return changeArr;
-    },
-
-    stripQuotes: function(start) {
-        return start.replace(/['"]/g, '');
-    },
-
-    scramble: function(start) {
-        // Fisher Yates shuffle from http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-        var shuffleArr = start.split('');
-        for(var i = 0; i < start.length; i++) {
-            j = Math.floor(Math.random() * i);
-            var shuffleI = shuffleArr[i];
-            shuffleArr[i] = shuffleArr[j];
-            shuffleArr[j] = shuffleI;
-        }
-        return shuffleArr.join('');
-    },
-
-    powersOfTwo: function(limit, callback) {
-        var num = 1;
-        while(num <= limit) {
-            callback(num);
-            num*=2;
-        }
-    },
-
-    prefixes: function(str, callback) {
-        var strArr = str.split('');
-        for(var i = 0; i <= strArr.length; i++) {
-            callback(strArr.slice(0, i).join(''));
-        }
-    },
-
-    interleave: function(arr1, arr2) {
-        // TODO: This is really ugly. Fix.
-        var onArr1 = true;
-        var returnArr = [];
-        shortestLength = Math.min(arr1.length, arr2.length);
-        for(var i = 0; i < shortestLength * 2; i++) {
-            var index = Math.floor(i/2);
-            returnArr.push(onArr1 ? arr1[index] : arr2[index]);
-            onArr1 = !onArr1;
-        }
-        returnArr = returnArr.concat(arr1.slice(shortestLength));
-        returnArr = returnArr.concat(arr2.slice(shortestLength));
-        return returnArr;
-    },
-
-    stutter: function(arr) {
-        return this.interleave(arr, arr);
+exports.change = function(amount) {
+    if (amount < 0) {
+        throw new RangeError('amount cannot be negative');
     }
-};
+    var changeArr = [];
+    var quarterVal = 25,
+        dimeVal = 10,
+        nickelVal = 5;
+    value = amount;
+    changeArr.push(Math.floor(value / quarterVal));
+    value %= quarterVal;
+    changeArr.push(Math.floor(value / dimeVal));
+    value %= dimeVal;
+    changeArr.push(Math.floor(value / nickelVal));
+    changeArr.push(value % nickelVal);
+    return changeArr;
+},
+
+exports.stripQuotes = function(str) {
+    return str.replace(/['"]/g, '');
+},
+
+exports.scramble = function(start) {
+    // Fisher Yates shuffle from http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    var shuffleArr = start.split('');
+    for(var i = 0; i < start.length; i++) {
+        var j = Math.floor(Math.random() * i);
+        var shuffleI = shuffleArr[i];
+        shuffleArr[i] = shuffleArr[j];
+        shuffleArr[j] = shuffleI;
+    }
+    return shuffleArr.join('');
+},
+
+exports.powersOfTwo = function(limit, callback) {
+    for (var x = 1; x <= limit; x *= 2) {
+        callback(x);
+    }
+},
+
+exports.prefixes = function(str, callback) {
+    var strArr = str.split('');
+    for(var i = 0; i <= strArr.length; i++) {
+        callback(strArr.slice(0, i).join(''));
+    }
+},
+
+exports.interleave = function(a, b) {
+    var aLen = a.length,
+        bLen = b.length,
+        maxLen = Math.max(aLen, bLen),
+        returnArr = [];
+    for (var i = 0; i <  maxLen; i++) {
+        if (i < aLen) {
+            returnArr.push(a[i]);
+        }
+        if (i < bLen){
+            returnArr.push(b[i]);
+        }
+    }
+    return returnArr;
+},
+
+exports.stutter = function(arr) {
+    return this.interleave(arr, arr);
+}
